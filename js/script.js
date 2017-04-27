5e579b46f7cf26a60796416fb48a52f0bf202cb0
Takahiro KASHIWAGI <takahirokashiwagi.cassy@gmail.com>
2017-04-27 19:40:36 +0900

    upload loading and show success

--- js/script.js
+++ js/script.js
@@ -10,10 +10,20 @@ $(function(){
       if (!input.val) $(input).val(name);
     });
   });
+
+  var hash = location.hash;
+  if (hash.indexOf('upload_photo') > 0) {
+    if (hash.indexOf('success') > 0) {
+      show_success($('#album_form form'));
+    } else {
+      show_fail($('#album_form form'));
+    }
+    location.hash = 'album_form';
+  }
 });
 
 function submit_attend_form(form) {
-  send_form($form);
+  send_form($(form));
   return false;
 }
 
@@ -27,12 +37,33 @@ function submit_album_form(form) {
 
 // フォーム送信
 function send_form($form) {
+  $('.alert', $form).hide();
+  $('.loading', $form).removeClass('hidden');
   $.ajax({
     type: 'POST',
     url: 'https://script.google.com/macros/s/AKfycbzIIEPV_mv1Bk-jA12ZHXZk49T_-3_92YuMVbGBSVwl273pSlQ/exec',
     data: $form.serialize(),
     dataType: 'jsonp'
-  }).done(function(res){
-    console.log(res);
+  }).done(function(res) {
+    show_success($form);
+  }).fail(function(e) {
+    show_fail($form);
+  }).always(function(xhr, st) {
+    refresh_form($form);
+    $('.loading', $form).addClass('hidden');
   });
+}
+
+function refresh_form($form) {
+  $('.j_input', $form).val('');
+}
+
+function show_success($form) {
+  $('.alert', $form).hide();
+  $('.success.alert', $form).show();
+}
+
+function show_fail($form) {
+  $('.alert', $form).hide();
+  $('.fail.alert', $form).show();
 }
--- upload.php
+++ upload.php
@@ -1,12 +1,13 @@
 <?php
 
-if (is_uploaded_file($_FILES["image"]["tmp_name"])) {
-  if (move_uploaded_file($_FILES["image"]["tmp_name"], "files/" . $_FILES["image"]["name"])) {
-    chmod("files/" . $_FILES["image"]["name"], 0644);
-    header("Location: " . $_SERVER['HTTP_REFERER'] . "#upload=success");
+if (is_uploaded_file($_FILES['image']['tmp_name'])) {
+  $file = 'files/' . date('Ymd_Hi_') . $_FILES['image']['name'];
+  if (move_uploaded_file($_FILES['image']['tmp_name'], $file)) {
+    chmod($file, 0644);
+    header('Location: ' . $_SERVER['HTTP_REFERER'] . '#upload_photo=success');
   } else {
-    header("Location: " . $_SERVER['HTTP_REFERER'] . "#upload=fail");
+    header('Location: ' . $_SERVER['HTTP_REFERER'] . '#upload_photo=fail');
   }
 } else {
-  header("Location: " . $_SERVER['HTTP_REFERER'] . "#upload=not_found");
+  header('Location: ' . $_SERVER['HTTP_REFERER'] . '#upload_photo=not_found');
 }
