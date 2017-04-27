590db2250467e445495322b4107fb0324cb7a117
Takahiro KASHIWAGI <takahirokashiwagi.cassy@gmail.com>
2017-04-28 00:28:55 +0900

    fix files.php

--- files.php
+++ files.php
@@ -1,30 +1,13 @@
 <?php
-  // ディレクトリのパスを記述
-  $dir = "files/" ;
+$dir = "files/" ;
 
-  // ディレクトリの存在を確認し、ハンドルを取得
-  if( is_dir( $dir ) && $handle = opendir( $dir ) ) {
-    // [ul]タグ
-    echo "<ul>" ;
-
-    // ループ処理
-    while( ($file = readdir($handle)) !== false ) {
-      // ファイルのみ取得
-      if( filetype( $path = $dir . $file ) == "file" ) {
-        /********************
-
-          各ファイルへの処理
-
-          $file ファイル名
-          $path ファイルのパス
-
-        ********************/
-
-        // [li]タグ
-        echo "<li><img src='" . $path . "'></li>" ;
-      }
+if( is_dir( $dir ) && $handle = opendir( $dir ) ) {
+  echo "<ul>" ;
+  while( ($file = readdir($handle)) !== false ) {
+    if( filetype( $path = $dir . $file ) == "file" ) {
+      // [li]タグ
+      echo "<li><img src='" . $path . "' width='300px'></li>" ;
     }
-
-    // [ul]タグ
-    echo "</ul>" ;
   }
+  echo "</ul>" ;
+}
