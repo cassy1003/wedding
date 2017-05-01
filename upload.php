2a3d4b311cf2b3c82474f8cc98b0e9332a349ce2
Takahiro KASHIWAGI <takahirokashiwagi.cassy@gmail.com>
2017-05-01 22:19:21 +0900

    upload photo files

--- upload.php
+++ upload.php
@@ -1,14 +1,28 @@
 <?php
+
 try {
   $valid = false;
-  foreach($_FILES['images']['tmp_name'] as $index => $tmp) {
-    //if (!is_null($tmp) && is_uploaded_file($tmp) {
+  $images = $_FILES['images'];
+  foreach($images['tmp_name'] as $index => $tmp) {
     $file = 'files/' . date('YmdHi_') . $_POST['email'] . '_' . $_FILES['images']['name'][$index];
-    if (move_uploaded_file($tmp, $file)) {
+    //if (!is_null($tmp) && is_uploaded_file($tmp) {
+    $exif = exif_read_data($tmp, 0, true);
+    switch ($exif['IFD0']['Orientation']) {
+    case 3:
+      $created = createImage($tmp, $images['type'][$index], 180, $file);
+      break;
+    case 6:
+      $created = createImage($tmp, $images['type'][$index], 270, $file);
+      break;
+    case 8:
+      $created = createImage($tmp, $images['type'][$index], 90, $file);
+      break;
+    }
+
+    if ($created || move_uploaded_file($tmp, $file)) {
       chmod($file, 0644);
       $valid = true;
     }
-    //}
   }
   if ($valid) {
     header('Location: ' . $_SERVER['HTTP_REFERER'] . '#upload_photo=success');
@@ -17,4 +31,24 @@ try {
   }
 } catch(Exception $e) {
   header('Location: ' . $_SERVER['HTTP_REFERER'] . '#upload_photo=fail');
+}
+
+function createImage($tmp, $type, $angle, $file) {
+  switch ($type) {
+  case 'image/png':
+    $img = ImageCreateFromPNG($tmp);
+    break;
+  case 'image/jpg':
+  case 'image/jpeg':
+    $img = ImageCreateFromJPEG($tmp);
+    break;
+  }
+  $created = false;
+  if ($img) {
+    $rotate = imagerotate($img, $angle, 0);
+    $created = imagejpeg($rotate, $file);
+    imagedestroy($img);
+    imagedestroy($rotate);
+  }
+  return $created;
 }
